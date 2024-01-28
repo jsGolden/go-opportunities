72202d17d38f71562f6a71ae8f3281a556b37d40
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-01-28 18:21:37 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -9,7 +11,16 @@ var (
 )
 
 func Init() error {
+	var err error
+	db, err = InitializeSQLite()
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(prefix string) *Logger {
