8cd5574e58b116bfd5b9793f178de92f6e73dfd1
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-01-28 22:19:19 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/jsGolden/go-opportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
