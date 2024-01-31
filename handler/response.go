ed8f39afe4db39cd71d73fead24e71ef00ef525b
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-01-31 11:28:44 -0300

    feat: add all process CRUD logic

--- handler/response.go
+++ handler/response.go
@@ -16,7 +16,7 @@ func sendError(ctx *gin.Context, code int, msg string) {
 
 func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprint("operation from handler %s successfull", op),
+		"message": fmt.Sprintf("operation from handler %s successfull", op),
 		"data":    data,
 	})
 }
--- handler/showOpening.go
+++ handler/showOpening.go
@@ -4,10 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jsGolden/go-opportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("/:id", "urlParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
